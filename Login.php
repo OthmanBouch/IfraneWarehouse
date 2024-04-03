61f004a7424542bc4d6659fbd2f650f4e8dc9c2b
OthmanBouch <Ot.Bouchentouf@aui.ma>
2024-04-03 01:59:18 +0000

    third try

--- Login.php
+++ Login.php
@@ -48,7 +48,7 @@ if(isset($_POST['submit'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>IfraneWarehouse Log In</title>
-   <link rel="shortcut icon" type="x-icon" href="/images/loginfavicon.png">
+   <link rel="shortcut icon" type="x-icon" href="loginfavicon.png">
    <link rel="stylesheet" href="css/style.css">
 
 </head>
