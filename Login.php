bcfb09094978efebe2559a42b149b816b2c9055f
OthmanBouch <Ot.Bouchentouf@aui.ma>
2024-05-08 00:27:09 +0100

    adjust page name

--- Login.php
+++ Login.php
@@ -41,7 +41,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>IfraneWarehouse Log In</title>
+   <title>Log In</title>
    <link rel="shortcut icon" type="x-icon" href="AppFavicon.png">
    <link rel="stylesheet" href="css/style.css">
 
